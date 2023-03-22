49441695a6de73b14288fd26d5e98ef97a168c7b
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-03-22 10:54:16 +0300

    add answer the questions

---
+++ index.js
@@ -0,0 +1,123 @@
+function showMessage() {
+  alert("Привет Кот!");
+}
+
+//let age = prompt("Сколько тебе лет?"); //запишет строкой
+//console.log(`Тебе ${age} лет!`);
+
+// let yes = confirm('Продолжить программировать?');
+// alert('Вы выбрали '+yes);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(5, 10));
+
+// function y(x) {
+//   return x ** 2;
+// }
+// let result = y(3);
+// console.log(result);
+
+// function showMessageFromUser(name, message = "Пустой текст") {
+//   console.log(name + ": " + message);
+// }
+// showMessageFromUser("Оксана", "привет!");
+
+// (function (a, b) {
+//   console.log(a + b);
+// })(2, 5);
+
+// function test(func) {
+//   func(3, 5);
+// }
+// test(function (a, b) {
+//   console.log(a + b);
+// });
+
+// console.log(
+//   (function (a, b) {
+//     return a + b;
+//   })(4, 5)
+// );
+
+// let sum = (a, b) => a + b;
+// let result = sum(5, 5);
+// console.log(result);
+
+// let showMessageHi = () => console.log("Привет!");
+// showMessageHi();
+
+// let user = prompt("Как твое имя?");
+// console.log("Привет, " + user + "!");
+
+// function hiHuman(user) {
+//   console.log("Привет, " + user + "!");
+// }
+// hiHuman("Оксана");
+
+// let sum = (a, b) => a + b;
+// let result = sum(5, 10);
+// console.log(result);
+
+// (function (a, b) {
+//   console.log(a + b);
+// })(5, 10);
+
+// console.log(
+//   (function (a, b) {
+//     return a + b;
+//   })(5, 10)
+// );
+
+// function sum1(func) {
+//   func(5, 10);
+// }
+// sum1(function (a, b) {
+//   console.log(a + b);
+// });
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// let result = sum(5, 10);
+// console.log(result);
+
+// function y(x) {
+//   return x ** 2;
+// }
+// let result = y(3);
+// console.log(result);
+
+// function sum(a, b = 10) {
+//   console.log(a + b);
+// }
+// sum(5);
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// sum(5, 10);
+
+// let sum = (a, b) => console.log(a + b);
+// sum(5, 10);
+
+// function sum(a) {
+//   console.log(5 + a);
+// }
+// sum(10);
+
+// function sum(a) {
+//   return a + 10;
+// }
+// console.log(sum(5));
+
+// function showX(x) {
+//   return x;
+// }
+// console.log(showX(28));
+
+// function sum(x, y = 0) {
+//   return x + y;
+// }
+// console.log(sum(5));
